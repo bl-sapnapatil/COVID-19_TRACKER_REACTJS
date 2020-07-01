81b083721562c315af8bd98ef4a064c74a7597cf
shindepriti <pritishinde34@gmail.com>
2020-07-01 17:06:07 +0530

    ADDED: Initialize React Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy,Suspense } from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 
+const Navbar = lazy(()=>
+  import('./components/Navbar')
+)
 function App() {
+  const pages = 
+    {
+      pageLink:'/Navbar',
+      view:Navbar,
+      displayName:'Navbar',
+      
+    }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div />}>
+      <Router>
+        <div>
+      <Route path={pages.pageLink} render={({match}) => <pages.view />} />
+      </div>
+      </Router>
+      </Suspense>
     </div>
   );
 }
