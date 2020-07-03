b6c157b3406853557a1b5085252b802f9d270e41
shindepriti <pritishinde34@gmail.com>
2020-07-03 11:48:26 +0530

    MODIFIED:Make Correct Alignment of Side Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,36 +2,39 @@ import React from 'react'
 import {ListItemIcon,ListItemText} from '@material-ui/core'
 import '../scss/navbar.scss'
 import { Home ,Users,Package,HelpCircle, Book} from 'react-feather'
+import { Link } from 'react-router-dom'
 class Navbar extends React.Component{
     render(){
         return(
-           <div>
-               <div className="navbar">
-                    <div>
-                        <ListItemIcon><Home/></ListItemIcon>
-                        <ListItemText>Home</ListItemText>
+           <div className="navbar">
+               <div className="navbar-logo">
+                       COVID19<span>INDIA</span>                   
+               </div>
+
+               <div className="navbar-menulist">
+                    <div className="navbar-menulist-item">
+                        <span><Home/></span>
+                        <span className="name">Home</span>
                     </div>
-                    <div>
-                        <ListItemIcon><Users/></ListItemIcon>
-                        <ListItemText>Demographics</ListItemText>
+                    <div className="navbar-menulist-item">
+                        <span><Users/></span>
+                        <span className="name">Demographics</span>
                     </div>
-                    <div>
-                        <ListItemIcon><Package/></ListItemIcon>
-                        <ListItemText>Essentials</ListItemText>
+                    <div className="navbar-menulist-item">
+                        <span><Package/></span>
+                        <span className="name">Essentials</span>
                     </div>
-                    <div>
-                        <ListItemIcon><HelpCircle/></ListItemIcon>
-                        <ListItemText>About</ListItemText>
+                    <div className="navbar-menulist-item">
+                        <span><HelpCircle/></span>
+                        <span className="name">About</span>
                     </div>
-                    <div>
-                        <ListItemIcon><Book/></ListItemIcon>
-                        <ListItemText>Blog</ListItemText>
+                    <div className="navbar-menulist-item">
+                        <span><Book/></span>
+                        <span className="name">Blog</span>
                     </div>
                </div>               
            </div>
-            
         )
-
     }
 } 
 export default Navbar
